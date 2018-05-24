05ba07114f402842186eed90a287450989342bac
yuemingdeng <391316512@qq.com>
2018-05-25 02:21:08 +0800

    测试使用

--- src/com/first/BinarySearch.java
+++ src/com/first/BinarySearch.java
@@ -1,37 +1,16 @@
 package com.first;
 
+import java.util.List;
+import java.util.Vector;
+
 /**
  * @author yuemingdeng
  * @ time 2018/5/25 1:44
  */
 public class BinarySearch {
-    public static int partition(int[] arr,int low,int high){
-            int temp=arr[low];
-            while(low<high){
-                while(arr[high]<=temp&&high>low)
-                    --high;
-                arr[low]=arr[high];
-                while(arr[low]>=temp&&low<high)
-                    ++low;
-                arr[high]=arr[low];
-            }
-            arr[high]=temp;
-            return high;
-        }
-        public static void find_k(int k,int[] arr,int low,int high){
-            int temp=partition(arr,low,high);
-            if(temp==k-1){
-                System.out.print("第"+k+"大的数是："+arr[temp]);
-            }else if(temp>k-1){
-                find_k(k,arr,low,temp-1);
-            }else{
-                find_k(k-temp,arr,temp+1,high);
-            }
-        }
+    private BinarySearch() {}
+    public static void set(String key, Object value) {
+    }
+    List<Integer> sub = new Vector<Integer>();
 
-
-        public static void main(String[] args) {
-            int[] arr={3,1,2,5,4,7,6};
-            find_k(2,arr,0,arr.length-1);
-        }
 }
